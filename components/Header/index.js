2bbf147897586f0c062fcdf810da0b299058649a
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-12 18:39:02 -0500

    Accidentally merged directly to master

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 // STEP 1: Create a header component.
 // -----------------------
 // Using a function create the component you see below:
@@ -10,45 +9,4 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-const headerData = {
-  date: 'SMARCH 28, 2019',
-  title: 'Lambda Times',
-  temp: '98°',
-};
-
-function Header() {
-  /* Creating elements per the provided HTML structure */
-  const headerWrapper = document.createElement('div');
-  const headerDate = document.createElement('span');
-  const headerTitle = document.createElement('h1');
-  const headerTemp = document.createElement('span');
-
-  /* Adding classNames to the elements that were created */
-  headerWrapper.classList.add('header');
-  headerDate.classList.add('date');
-  headerTemp.classList.add('temp');
-
-  /* Adding content to the elements that were created */
-  headerDate.textContent = `${headerData.date}`;
-  headerTitle.textContent = `${headerData.title}`;
-  headerTemp.textContent = `${headerData.temp}`;
-  /*
-  # OLD Content
-  headerDate.textContent = 'SMARCH 28, 2019';
-  headerTitle.textContent = 'Lambda Times';
-  headerTemp.textContent = '98°';
-  */
-
-  /* Appending the elements that were created ( children ) to {headerWrapper} ( parent ) */
-  headerWrapper.appendChild(headerDate);
-  headerWrapper.appendChild(headerTitle);
-  headerWrapper.appendChild(headerTemp);
-
-  /* Return the element created */
-  return headerWrapper;
-}
-
-/* Using {querySelector} to assign the element with the className of 'header-container' to {headerContainer} */
-const headerContainer = document.querySelector('.header-container');
-/* Appending {Header} ( child ) with no params to {headerContainer} ( parent ) */
-headerContainer.appendChild(Header());
+function Header() {}
